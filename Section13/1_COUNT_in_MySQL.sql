b204d46855471d6d217c1d139d93cfb2e9c580a1
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2025-03-27 20:38:39 +0530

    First commit

--- Section13/1_COUNT_in_MySQL.sql
+++ Section13/1_COUNT_in_MySQL.sql
@@ -1,3 +1,8 @@
 
 select count(*) from sales;
 -- The above query gives the count of the total rows in the database.
+
+
+select count(order_line) as 'Number of Products ordered', count(distinct order_id) as 'Number of orders' from sales;
+
+select count(order_line) as 'Number of Products ordered', count(distinct order_id) as 'Number of orders' from sales where customer_ID='cg-12520';
---
+++ Section13/2_SUM_in_MySQL.sql
@@ -0,0 +1,5 @@
+select sum(profit) as 'Total Profit' from sales;
+
+select * from sales;
+
+select sum(quantity) as 'Total Quantity' from sales where Product_ID='FUR-TA-10000577';
